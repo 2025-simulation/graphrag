a48c260ca02874e33bce42227e66df7bcb2b439d
Kazure Zheng <kazure@debian>
2025-10-10 19:09:59 +0800

    some modify

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
-apt update && apt install uv
-uv venv --python 3.12 && source .venv/bin/activate && uv pip install -r requirements.txt
+#!/bin/sh
+
+echo "[STARTUP] apt updating..."
+apt update
+
+echo "[STARTUP] installing uv..."
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+echo "[STARTUP] setting up the uv environment..."
+uv venv --python 3.12 && /app/graphrag/.venv/python -m pip install -r requirements.txt
